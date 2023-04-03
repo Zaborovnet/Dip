71e806ac3b9f9b33a698432388dbba6cd7c46d19
Zaborovnet <Avdeev02850285@yandex.ru>
2023-04-03 03:30:19 +0300

    Починка PredictServer для кидания на него запросов

--- PredictServer/Table.py
+++ PredictServer/Table.py
@@ -20,11 +20,13 @@ rows = cur.fetchall()
 for row in rows:
     print(row)
 url = 'http://localhost:5000/'
-filename = "0.png"
-data = {'number': 1, 'longitude': 25, 'latitude': 123, 'image': open(filename, "rb"), 'time': str(datetime.datetime.now())}
+filename = '0.png'
 
-response = requests.post(url, data=data)
-
+#data = {'number': 123, 'longitude': 45.123, 'latitude': -123.456, 'image': open(filename, "rb"), 'time': str(datetime.datetime.now())}
+response = requests.post('http://localhost:5000/', data={'number': 1, 'longitude': 45.678, 'latitude': 32.123, 'time': str(datetime.datetime.now()), 'type': 'unknown'}, files={'image': open('0.png', 'rb')})
+#response = requests.post(url, data=data)
+print(response.status_code)
+print(response.text)
 if response.status_code == 200:
     print('User added successfully')
 else:

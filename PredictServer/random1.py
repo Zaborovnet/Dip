dd0a06beb111cfac2d54c4bf4ff4760631497c72
Zaborovnet <Avdeev02850285@yandex.ru>
2023-04-04 05:43:30 +0300

    12

--- PredictServer/random1.py
+++ PredictServer/random1.py
@@ -9,13 +9,14 @@ with open('0.png', 'rb') as file:
 url = 'http://localhost:5000/'
 headers = {'Content-Type': 'application/json'}
 # создание нового объекта
-data = {     "number": 123,
-             "longitude": 55.755826,
-             "latitude": 37.6172999,
-             "time": str(datetime.datetime.now()) }
-files = {     'image': img_data }
-response = requests.post(url, data=data, files=files)
-print(response.status_code)
+for i in range(10):
+    data = {     "number": i,
+                 "longitude": 55.755826,
+                 "latitude": 37.6172999,
+                 "time": str(datetime.datetime.now()) }
+    files = {     'image': img_data }
+    response = requests.post(url, data=data, files=files)
+    print(response.status_code)
 # изменение координат уже существующих объектов
 response = requests.get('http://localhost:5000/')
 if response.status_code == 200:
@@ -25,8 +26,8 @@ if response.status_code == 200:
             print (targets)
             print (targets)
             #генерация новых координат
-            new_longitude = target['longitude'] + random.uniform(-0.001, 0.001)
-            new_latitude = target['latitude'] + random.uniform(-0.001, 0.001)
+            new_longitude = target['longitude'] + random.uniform(-0.01, 0.01)
+            new_latitude = target['latitude'] + random.uniform(-0.01, 0.01)
              #отправка запроса на изменение координат
             data = {
                 "number": target['number'],
